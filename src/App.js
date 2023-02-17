cd77a037558500e9925bb847a4abd63ed75f109d
luvgameon <luvgameon@gmail.com>
2023-02-17 17:17:08 +0530

    Meal Summary added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 import React from 'react';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header title="ReactMeal"/>
+      <Meals/>
     </div>
   );
 }
